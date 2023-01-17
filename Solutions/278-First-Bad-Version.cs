84814528c387bf965e769b6dbd8be2eb01cebc69
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-16 21:27:18 -0300

    Update 278-First-Bad-Version.cs

--- Solutions/278-First-Bad-Version.cs
+++ Solutions/278-First-Bad-Version.cs
@@ -3,9 +3,10 @@ public class Solution : VersionControl {
     public int FirstBadVersion(int n) {
         if(IsBadVersion(1) || n == 1) return 1;
         int left = 1, right = n;
+        int middle =  left + (right - left) / 2;
         while(left < right)
         {   
-            int middle = left + (right - left) / 2;
+            middle = left + (right - left) / 2;
             if(IsBadVersion(middle)) right = middle;
             else left = middle + 1;
         }

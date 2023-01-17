eb081bf35a9e36b93e759d1e680d125a31c4e5c3
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-16 22:31:06 -0300

    Create 167-Two-Sum-II.cs

---
+++ Solutions/167-Two-Sum-II.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int leftPointer = 0,
+            rightPointer = numbers.Length -1;
+        var middle = 0;
+        while(leftPointer <= rightPointer)
+        {
+            middle = left + ( (right - left) / 2);
+            if(numbers[middle] > target)
+                
+        }
+    }
+}

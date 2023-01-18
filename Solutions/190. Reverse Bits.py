dfbeabaeba891667a7461a3ccfdb43571f60433f
Abraão Állysson dos Santos Honório <abraaohonoriopb@gmail.com>
2023-01-17 22:37:31 -0300

    Update 190. Reverse Bits.py

--- Solutions/190. Reverse Bits.py
+++ Solutions/190. Reverse Bits.py
@@ -1,6 +1,5 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        stack = []
         binary = f'{n:032b}'
         reverse_binary = binary[::-1]    
         return int(reverse_binary,2)

de3cab98f1bfd21e2c8f74f0f6378534074f2f49
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-20 21:16:36 -0300

    Update 231-Power-of-Two.cs

--- Solutions/231-Power-of-Two.cs
+++ Solutions/231-Power-of-Two.cs
@@ -1,3 +1,10 @@
+// 20/01/2023 - review
+public class Solution {
+    public bool IsPowerOfTwo(long x) 
+        // => (x is not 0) && (x & (x - 1)) == 0;
+        => (x is not 0) && (x & (~x +1)) == x;
+}
+
 // first solution
 public class Solution {
     public bool IsPowerOfTwo(int n) {

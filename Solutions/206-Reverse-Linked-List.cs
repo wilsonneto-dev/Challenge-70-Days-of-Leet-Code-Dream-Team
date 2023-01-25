6c409b48e75f32b3d8b3617381b09b197d5a6c4d
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-01-24 21:29:54 -0300

    Update 206-Reverse-Linked-List.cs

--- Solutions/206-Reverse-Linked-List.cs
+++ Solutions/206-Reverse-Linked-List.cs
@@ -1,4 +1,5 @@
 
+// recursive with two parameters
 public class Solution {
     public ListNode ReverseList(ListNode head, ListNode? last = null) {
         if(head is null) return last;
@@ -6,5 +7,19 @@ public class Solution {
         var next = head.next;
         head.next = last;
         return ReverseList(next, head);
+    }
+}
+
+// response recursive
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head?.next is null) return head;
+
+        var reversedSublist = ReverseList(head.next);
+        
+        head.next.next = head;
+        head.next = null;
+
+        return reversedSublist;    
     }
 }
